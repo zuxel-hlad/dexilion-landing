f8ec35d9a99b4449f393ecd4230b3449156af50c
Stanislav <stanislavschelokov@gmail.com>
2021-08-30 18:34:54 +0300

    created custom one page scroll

--- js/script.js
+++ js/script.js
@@ -1,35 +1,18 @@
-// new WOW().init();
-
-let index = 0;
-
-const sections = document.querySelectorAll('section');
-let pageHeight = window.innerHeight;
-
+const pageHeight = window.innerHeight;
+let yPos = '0px';
 let isAnimating = false;
-
 document.body.style.transform = 'translate3d(0px,0px,0px)';
+
 document.addEventListener('scroll', function (e) {
   document.body.scrollTop = 0;
 });
 document.addEventListener('wheel', wheelListener);
 
-function positivHeight() {
-  index++;
-  return parseInt(getComputedStyle(sections[index]).height);
-}
-
-function negativeHeight() {
-  index--;
-  return parseInt(getComputedStyle(sections[index]).height);
-}
-
 function wheelListener(e) {
   if (e.deltaY > 0) {
     scrollPage(-pageHeight);
-    console.log('scroll down');
   } else {
     scrollPage(+pageHeight);
-    console.log('scroll top');
   }
 }
 
@@ -38,14 +21,13 @@ function scrollPage(scrollSize) {
     return;
   }
   isAnimating = true;
-  let yPos = getNewYPos(scrollSize);
+  yPos = getNewYPos(scrollSize + 100);
   document.body.style.transform = 'translate3d(0px,' + yPos + ',0px)';
 }
 
 function getNewYPos(add) {
   let oldYPos = document.body.style.transform.split(',')[1];
   oldYPos = parseInt(oldYPos.replace(/px/, ''));
-
   let newYPos = oldYPos + add;
   if (newYPos > 0) {
     isAnimating = false;
