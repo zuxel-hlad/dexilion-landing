9a0c84fa793f6b2822f1898af6feba0e5389e5d8
stanislav <stanislavschelokov@gmail.com>
2021-09-01 19:07:50 +0300

    creating custom unimation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-const page = new fullpage('.fullpage-wrapper', {
+new fullpage('.fullpage-wrapper', {
   licenseKey: 'YOUR_KEY_HERE',
   scrollOverflow: true,
-  scrollingSpeed: 1500,
+  scrollingSpeed: 1000,
   css3: true,
   easingcss3: 'ease-out',
+  normalScrollElements: '#dexilion-features',
 });
