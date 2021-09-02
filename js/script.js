786d6a0f35c9fedb502eb120527ea9bcf6652f4c
Stanislav <stanislavschelokov@gmail.com>
2021-09-03 00:57:22 +0300

    added animation to header. fixed dexilion-round animation'

--- js/script.js
+++ js/script.js
@@ -2,15 +2,13 @@ let $isAnimatedEl = $('.hero-text');
 
 new fullpage('.fullpage-wrapper', {
   licenseKey: 'YOUR_KEY_HERE',
-  scrollOverflow: false,
-  scrollingSpeed: 900,
+  scrollOverflow: true,
+  scrollingSpeed: 1200,
   css3: true,
   easingcss3: 'ease-out',
   normalScrollElements: '#dexilion-features',
-  afterLoad: function (index, nextIndex, direction) {
-    console.log(index);
-    if (index.index == 0) {
-      $isAnimatedEl.addClass('show');
-    }
-  },
 });
+
+const round = document.querySelector('.hero-img-round');
+
+setTimeout(() => round.classList.add('active'), 2000);
