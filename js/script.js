8e9c10e0827b259e0c3cffc1683735fb25b621db
stanislav <stanislavschelokov@gmail.com>
2021-09-02 19:00:18 +0300

    fixing scroll bug

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
+let $isAnimatedEl = $('.hero-text');
+
 new fullpage('.fullpage-wrapper', {
   licenseKey: 'YOUR_KEY_HERE',
-  scrollOverflow: true,
+  scrollOverflow: false,
   scrollingSpeed: 900,
   css3: true,
   easingcss3: 'ease-out',
   normalScrollElements: '#dexilion-features',
+  afterLoad: function (index, nextIndex, direction) {
+    console.log(index);
+    if (index.index == 0) {
+      $isAnimatedEl.addClass('show');
+    }
+  },
 });
