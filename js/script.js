a3234efca416c2940d869196c75a047aa8748edd
stanislav <stanislavschelokov@gmail.com>
2021-09-02 12:28:31 +0300

    changed project structure, added custom logo to hero section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 new fullpage('.fullpage-wrapper', {
   licenseKey: 'YOUR_KEY_HERE',
   scrollOverflow: true,
-  scrollingSpeed: 1000,
+  scrollingSpeed: 900,
   css3: true,
   easingcss3: 'ease-out',
   normalScrollElements: '#dexilion-features',
