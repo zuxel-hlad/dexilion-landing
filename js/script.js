c49c0b67505962acfa92701f7931c8772c5ca8a0
Stanislav <stanislavschelokov@gmail.com>
2021-09-08 22:34:33 +0300

    added header responsive and mobile menu

--- js/script.js
+++ js/script.js
@@ -338,3 +338,30 @@ $(document).ready(function () {
     },
   });
 });
+
+/* mobile burger */
+
+const burger = document.querySelector('.burger-menu'),
+  headerWrapper = document.querySelector('.header-nav-wrapper');
+
+const mobileMenuToggler = () => {
+  burger.classList.toggle('active');
+  headerWrapper.classList.toggle('active');
+};
+
+burger.addEventListener('click', (e) => {
+  e.stopPropagation();
+  mobileMenuToggler();
+});
+
+/* if mobile menu open and click outside */
+document.addEventListener('click', (e) => {
+  let target = e.target;
+  let its_menu = target == headerWrapper || headerWrapper.contains(target);
+  let its_hamburger = target == burger;
+  let menu_is_active = headerWrapper.classList.contains('active');
+
+  if (!its_menu && !its_hamburger && menu_is_active) {
+    mobileMenuToggler();
+  }
+});
