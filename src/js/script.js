8c0843694fe672b82f04537cc2af1e78d2bba7d9
stanislav <stanislavschelokov@gmail.com>
2021-09-13 14:40:40 +0300

    added anchors to sections

--- src/js/script.js
+++ src/js/script.js
@@ -414,3 +414,24 @@ $('.roadmap-progress-slider').slick({
 });
 
 progressMobileItem.forEach((item, index) => {});
+
+
+// $(function(){
+//   $('a[href^="#"]').on('click', function(event) {
+//     // отменяем стандартное действие
+//     event.preventDefault();
+    
+//     var sc = $(this).attr("href"),
+//         dn = $(sc).offset().top;
+//     /*
+//     * sc - в переменную заносим информацию о том, к какому блоку надо перейти
+//     * dn - определяем положение блока на странице
+//     */
+    
+//     $('html, body').animate({scrollTop: dn}, 1000);
+    
+//     /*
+//     * 1000 скорость перехода в миллисекундах
+//     */
+//   });
+// });
